1860f7f7450f01403b3ebe45d4aff91df988a9f0
Fjdwe19 <143691757+Fjdwe19@users.noreply.github.com>
2025-06-16 19:43:32 +0700

    Update vite.config.js

--- kitafit_frontend/vite.config.js
+++ kitafit_frontend/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/kitafit_full/',
-});
+export default defineConfig(({ command }) => {
+  return {
+    base: command === 'build' ? '/kitafit_full/' : '/', 
+    plugins: [react()],
+  }
+})
